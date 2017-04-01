5326433846ad77fe3eb5a96f3abaa797c4bac09c
Bennett A <bennett.adams@jspargo.com>
2017-04-01 17:08:12 -0400

    Add Swipe ability

--- index.android.js
+++ index.android.js
@@ -15,71 +15,51 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import Swiper from 'react-native-swiper';
 
 
-var SCREEN_WIDTH = require('Dimensions').get('window').width;
-var BaseConfig = Navigator.SceneConfigs.FloatFromRight;
 
-var CustomLeftToRightGesture = Object.assign({}, BaseConfig.gestures.pop, {
-  // Make it snap back really quickly after canceling pop
-  snapVelocity: 8,
-  // Make it so we can drag anywhere on the screen
-  edgeHitWidth: SCREEN_WIDTH,
-});
 
-var CustomSceneConfig = Object.assign({}, BaseConfig, {
-  // A very tighly wound spring will make this transition fast
-  springTension: 100,
-  springFriction: 1,
-  // Use our custom gesture defined above
-  gestures: {
-    pop: CustomLeftToRightGesture,
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
+    backgroundColor:'transparent',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingHorizontal:30,
+    paddingVertical:30,
+    resizeMode: 'cover'
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: '700',
+    margin: 0,
+    color:"#fff",
   }
-});
+  ,
+  subheader: {
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '300',
+    marginBottom: 30,
+    color:"#ffb8b8",
+  }
+  ,
+  welcome: {
+    fontSize: 20,
+    textAlign: 'left',
+    fontWeight: '300',
+    margin: 15,
+    color:"#fe8d8d",
+  }
 
-var PageOne = React.createClass({
-  _handlePress() {
-    this.props.navigator.push({id: 2,});
-  },
+})
 
-  render() {
-    return (
-      <Image
-        source={require('./images/login-background.png')}
-        style={styles.container}>
-
-        <Text style={styles.header}>Get Your Life in Order</Text>
-        <Text style={styles.subheader}>Store all your files in a single, secure place, to be released to the person of your choice</Text>
-        <TouchableOpacity onPress={this._handlePress}>
-          <View style={{paddingVertical: 1, paddingHorizontal: 60, backgroundColor: 'white'}}>
-            <Text style={styles.welcome}>Get Started</Text>
-          </View>
-        </TouchableOpacity>
-
-     </Image>
-    )
-  },
-});
-
-var PageTwo = React.createClass({
-  _handlePress() {
-    this.props.navigator.pop();
-  },
-
-  render() {
-    return (
-      <Image source={require('./images/login-background2.png')}
-        style={styles.container}>
-        <Text style={styles.welcome}>This is page two!</Text>
-        <TouchableOpacity onPress={this._handlePress}>
-          <View style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'black'}}>
-            <Text style={styles.welcome}>Go back</Text>
-          </View>
-        </TouchableOpacity>
-       </Image>
-    )
-  },
-});
 
 class TestProject extends Component {
   _renderScene(route, navigator) {
@@ -103,46 +83,46 @@ class TestProject extends Component {
     );
   }
 }
+var swiper = React.createClass({
+  render: function() {
+    return (
+      <Swiper style={styles.wrapper} showsButtons={true}>
+         <Image
+        source={require('./images/onboard-1.png')}
+        style={styles.container}>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: undefined,
-    height: undefined,
-    backgroundColor:'transparent',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    paddingHorizontal:30,
-    paddingVertical:30,
-    resizeMode: 'cover'
-  },
-  header: {
-    fontSize: 20,
-    textAlign: 'center',
-    fontWeight: '700',
-    margin: 0,
-    color:"#e5bdbd",
-  }
-  ,
-  subheader: {
-    fontSize: 14,
-    textAlign: 'center',
-    fontWeight: '300',
-    marginBottom: 30,
-    color:"#de9c9c",
-  }
-  ,
-  welcome: {
-    fontSize: 26,
-    textAlign: 'center',
-    fontWeight: '700',
-    margin: 15,
-    color:"#9d3c3c",
-    borderRadius: 15,
-  }
-});
+          <Text style={styles.header}>Store all important Documents, Account Information and Passwords</Text>
+        <Text style={styles.subheader}>In our guaranteed secure, encrypted LifePreserver Vault</Text>
+        <TouchableOpacity onPress={this._handlePress}>
+          <View style={{paddingVertical: 1}}>
+            <Text style={styles.welcome}>Skip Intro</Text>
+          </View>
+        </TouchableOpacity>
+        </Image>
 
-AppRegistry.registerComponent('TestProject', () => TestProject);
+         <Image
+        source={require('./images/onboard-2.png')}
+        style={styles.container}>
+
+          <Text style={styles.header}>Store all important Documents, Account Information and Passwords</Text>
+        <Text style={styles.subheader}>In our guaranteed secure, encrypted LifePreserver Vault</Text>
+        <TouchableOpacity onPress={this._handlePress}>
+          <View style={{paddingVertical: 1}}>
+            <Text style={styles.welcome}>Skip Intro</Text>
+          </View>
+        </TouchableOpacity>
+        </Image>
+
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
+    )
+  }
+})
+
+
+AppRegistry.registerComponent('TestProject', () => swiper);
 
 module.exports = TestProject;
 

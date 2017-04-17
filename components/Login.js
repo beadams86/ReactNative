5cf11ed90c73ed8ba648abc39a184fa07339418a
Bennett A <bennett.adams@jspargo.com>
2017-04-17 11:03:50 -0400

    Latest commit with onboard and login pages

--- components/Login.js
+++ components/Login.js
@@ -16,58 +16,141 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    flexDirection: 'column',
   },
 
   backgroundImage:{
-	 flex:1,
 	 alignSelf:'stretch',
 	 width:  null,
-	 justifyContent: 'flex-end',
-	 alignItems: 'center',
-   resizeMode:'cover',
+   flex:1,
   },
-
-  introContent:{
-  	flexDirection:'row',
-    alignSelf:'stretch',
+  logoContainer:{
+    alignItems:'center',
+    flex:1,
+    padding:15,
+    marginTop:30,
   },
-
 
   textLink:{
   	padding:30,
   },
-  inputWrap:{
-    flex:1,
-    marginVertical:10,
-    height:40,
-    backgroundColor:'transparent'
+  inputWrapper:{
+    flex:2,
+    padding:15,
+    height:100, 
+    backgroundColor:'#fff',
+    flexDirection:'column',
+    alignSelf:'stretch'
+  },
+  txtLabel:{
+    color:'#666',
+    fontSize:16,
+    marginBottom:15,
+    fontWeight:'bold',
   },
   input:{
-    flex:1,
     paddingHorizontal:10,
     backgroundColor:"#fff",
-  }
+    marginBottom:30,
+    backgroundColor:'rgba(255,255,255,.7)',
+    borderBottomWidth:2,
+    borderBottomColor:"#fba2a2"
+  },
 
+  btnContainer:{
+    flexDirection:'row',
+    flexWrap:'wrap',
+    height:40,
+    marginBottom:15,
+  },
+
+  submitBtn:{
+    backgroundColor: '#ec3535',
+    padding:6,
+    flex:1,
+    marginRight:15,
+    borderRadius:50,
+  },
+  regBtn:{
+    borderWidth:1,
+    borderColor:'#ec3535',
+    flex:1,
+    padding:6,
+    borderRadius:50,
+  },
+  fbLogin:{
+    backgroundColor: '#2d4486',
+    padding:8,
+    borderRadius:50,
+  },
+  btnText:{
+    color:'#fff',
+    fontWeight:'bold',
+    fontSize:18,
+    alignSelf:'center'
+  },
+
+  registerTxt:{
+    fontSize:18,
+    color:'#ec3535',
+    alignSelf:'center',
+  },
+
+  txtDivider:{
+    marginBottom:15,
+    alignSelf:'center',
+    color:'#999',
+    fontSize:18,
+    fontStyle:'italic',
+  },
+  textLink:{
+    alignSelf:'center',
+    margin:15,
+    flexDirection:'row',
+  },
+  skipintro:{
+    color:'#ec3535'
+  }
 })
 export default class Login extends Component{
 	render(){
 		return(
 			 	 <View style={styles.container}>
 
-              <Image source={require('../images/login.png')} style={styles.backgroundImage}>
+              <Image source={require('../images/red-bg.png')} style={styles.backgroundImage}>
+                  <View style={styles.logoContainer}>
+                  <Image source={require('../images/LP-splashlogo.png')} />
+      				    </View>
 
-      				
-      						<View>
-  								<Text style={styles.headerText}>Login</Text>
+                  <View style={styles.inputWrapper}>
         					<TextInput
-                  placeholder="Username"
+                  placeholder="Enter Email Address"
                   style={styles.input}
                   underlineColorAndroid="transparent"
                   />
-	        								<TouchableOpacity onPress={this.pop} style={styles.textLink}>
-	          										<Text style={styles.skipintro}>I'm not registered yet</Text>
-	        								</TouchableOpacity>
-      						</View>
+
+                  <TextInput
+                  placeholder="Enter Password"
+                  secureTextEntry={true} 
+                  style={styles.input}
+                  underlineColorAndroid="transparent"
+                  />
+                  <View style={styles.btnContainer}>
+                  <TouchableOpacity onPress={this.pop} style={styles.submitBtn}>
+                        <Text style={styles.btnText}>Log On</Text>
+                  </TouchableOpacity>
+                    <TouchableOpacity onPress={this.pop} style={styles.regBtn}>
+                        <Text style={styles.registerTxt}> Register</Text>
+                  </TouchableOpacity>
+                  </View>
+                  <Text style={styles.txtDivider}>or</Text>
+                  <TouchableOpacity onPress={this.pop} style={styles.fbLogin}>
+                        <Text style={styles.btnText}>Facebook Login</Text>
+                  </TouchableOpacity>
+                  
+  							
+                  </View>
+                  
       				</Image>
     			</View>
 
